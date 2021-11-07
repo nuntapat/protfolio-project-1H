41978170504bb2b30201e3adaf31ee8b71377794
lull <nuntapat.aru@gmail.com>
2021-11-07 20:01:29 +0700

    navbar

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>Web development and website promo</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
