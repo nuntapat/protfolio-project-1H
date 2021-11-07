8cff02d5ea875e34ea248f36a8dbd4dffbe05095
lull <nuntapat.aru@gmail.com>
2021-11-07 22:38:30 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ function App() {
 
   return (
     <>
- 
   <Navbar />
   <Header />
-  
   </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Typed from "react-typed";
 
 const Header = () => {
     return (
         <div className="header-wraper">
             <div className="main-info">
                 <h1>Web development and website promo</h1>
+                <Typed
+                className="typed-text"
+                strings={["Web Design","Web Developement","Facebook Ads SMM","Google Ads"]}
+                typeSpeed={40}
+                backSpeed={60}
+                />
             </div>
         </div>
     )
