260eb427b261d0caffb5bb2702404b63cbb0c668
lull <nuntapat.aru@gmail.com>
2021-11-08 13:44:18 +0700

    harder

--- src/App.js
+++ src/App.js
@@ -8,7 +8,27 @@ function App() {
 
   return (
     <>
-    <Particles />
+    <Particles 
+    className="particles-canvas"
+      params={{
+        particles : {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 900
+            }
+          },
+          shape:{
+            type: "circle",
+            stroke: {
+              width: 6,
+              color: "#f9ab00"
+            }
+          }
+        }
+      }}
+      />
     <Navbar />
     <Header />
   </>
