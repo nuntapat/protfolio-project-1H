0734a248ce4d7204afea2b9d82a24306112123d3
lull <nuntapat.aru@gmail.com>
2021-11-08 15:40:52 +0700

    About me Comp

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,13 +6,13 @@ const AboutMe = () => {
 
         <div className="container py-5">{
             <div className="row">
-                <div className="col-lg-6.col-xm-12">
-                    <div className="photo-wrap">
-                        <img src="profile-img" alt="author..." />
+                <div className="col-lg-6 col-xm-12">
+                    <div className="photo-wrap mb-5">
+                        <img className="profile-img" src={author} alt="author..." />
                     </div>
                 </div>
-                <div className="col-lg-6.col-xm-12">
-                    <h1>About Me</h1>
+                <div className="col-lg-6 col-xm-12">
+                    <h1 className="about-heading">About Me</h1>
                     <p>Hello! I am Nuntapat.I have been develoing website for 1 years. I'm Web Developer , Tester and Game Developer(UE Engine 4).
                         Technologies I use is MERN(MongoDB , Express , ReactJS and NodeJS).
                         In The Test Section I will use katalon studio and Selenium(Java language)
